b78ed9f1d407355a65ec0bbd8ddcb2c0f0ee65c4
Jared Lustig <70167622+jared-lustig@users.noreply.github.com>
2021-10-19 22:56:11 -0700

    Commiting again as the previous commit deleted some of the code I wrote. Additionally add some CSS styling, and added posts to index.

--- Term_Project/app/Controller/forms.py
+++ Term_Project/app/Controller/forms.py
@@ -1,4 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField, TextAreaField
+from wtforms.validators import DataRequired, Length
 
- 
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()]) #where the user types in the title
+    body = TextAreaField('Body', validators = [DataRequired()]) #where the user types in the body
+    submit = SubmitField('Post') #submit button
