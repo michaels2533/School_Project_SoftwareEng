dc583fbf06769541ad6f197e779da6830242bc8e
michaels2533 <michael.stawowski@wsu.edu>
2021-10-21 00:47:58 -0700

    Added Functionality to Apply for positon

--- Term_Project/app/Controller/forms.py
+++ Term_Project/app/Controller/forms.py
@@ -1,8 +1,17 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField, TextAreaField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, Email
 
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()]) #where the user types in the title
     body = TextAreaField('Body', validators = [DataRequired()]) #where the user types in the body
     submit = SubmitField('Post') #submit button
+
+
+class ApplicationForm(FlaskForm):
+    firstName = StringField('First', validators=[DataRequired()])
+    lastName = StringField('Last', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(),Email()])
+    phoneNum = StringField('Phone', validators = [DataRequired()])
+    body = TextAreaField('Resume', validators = [DataRequired()])
+    submit = SubmitField('Submit Application')
